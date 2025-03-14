0fcc48be801e9b962eb0f3060bfc5ac7fac0792f
Smita Rao <77341182+smitarao25@users.noreply.github.com>
2025-03-14 18:47:27 +0530

    Update test.java

--- test.java
+++ test.java
@@ -2,7 +2,6 @@ class test {
     public static void main(String[] args)
     {
         System.out.println("Hello, Smita");
-         System.out.println("Hello, Devops");
         System.out.println("HAPPY HOLI");
     }
 }
