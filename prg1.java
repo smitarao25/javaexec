49ad7147f39cf00261908bb8c405ff99fd8a8112
Smita Rao <77341182+smitarao25@users.noreply.github.com>
2025-03-19 18:49:20 +0530

    Update and rename test.java to prg1.java

---
+++ prg1.java
@@ -0,0 +1,7 @@
+class prg1 {
+    public static void main(String[] args)
+    {
+        System.out.println("All the best");
+      
+    }
+}
--- test.java
+++
@@ -1,7 +0,0 @@
-class test {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello, Smita");
-        System.out.println("HAPPY HOLI");
-    }
-}
